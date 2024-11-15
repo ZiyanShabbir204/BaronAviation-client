c2b945f429237e52fb7a996de7518290dd641bbc
Ammar Shabbir <ashabbir197@gmail.com>
2024-11-15 15:21:08 -0500

    changed address

--- src/components/pages/contact/Locations.jsx
+++ src/components/pages/contact/Locations.jsx
@@ -1,37 +1,36 @@
 const locations = [
   {
     id: 1,
-    title: "North America",
-    address: "1 Dundas St W Suite 2500 Toronto ON M5G 1Z3, Canada",
-    contact: "0-000-000-0000 canada@tourz.com",
+    title: "Karachi",
+    address: "Plot 10, Interactive Plaza, E-11/13, Islamabad, Pakistan",
+    contact: "0000-0000000",
   },
   {
     id: 2,
-    title: "Australia",
-    address: "123 Smith St, Melbourne, VIC 3000, Australia",
-    contact: "61-800-123-456 australia@tourz.com",
+    title: "Lahore",
+    address: "Plot 10, Interactive Plaza, E-11/13, Islamabad, Pakistan",
+    contact: "0000-0000000",
   },
   {
     id: 3,
-    title: "Europe",
-    address: "45 Rue du Faubourg Saint-Antoine, 75011 Paris, France",
-    contact: "33-800-987-654 europe@tourz.com",
-  },
-  {
-    id: 4,
-    title: "Asia",
-    address: "Tokyo, Japan",
-    contact: "81-800-567-890 asia@tourz.com",
+    title: "Islamabad",
+    address: "Plot 10, Interactive Plaza, E-11/13, Islamabad, Pakistan",
+    contact: "0000-0000000",
   },
 ];
 
 export default function Locations() {
   return (
     <section className="layout-pt-lg">
-      <div className="container">
+      <div
+        className="container"
+        style={{
+          maxWidth: "1000px",
+        }}
+      >
         <div className="row y-gap-30">
           {locations.map((elm, i) => (
-            <div key={i} className="col-lg-3 col-sm-6">
+            <div key={i} className="col-lg-4 col-sm-4">
               <div className="px-30 text-center">
                 <h3 className="text-30 md:text-24 fw-700 text-gradient-vivid-orange">
                   {elm.title}
@@ -39,7 +38,6 @@ export default function Locations() {
 
                 <div className="mt-20 md:mt-10">
                   {elm.address}
-                  <br />
                   <br />
                   {elm.contact}
                 </div>
