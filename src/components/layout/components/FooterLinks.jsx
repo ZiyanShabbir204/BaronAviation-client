3f86d76b15bd980b580acacf986cb2224098a69e
Ammar Shabbir <ashabbir197@gmail.com>
2024-11-16 10:57:42 -0500

    updated footer

--- src/components/layout/components/FooterLinks.jsx
+++ src/components/layout/components/FooterLinks.jsx
@@ -2,12 +2,8 @@ const sections = [
   {
     title: "Company",
     links: [
-      { id: 1, text: "About Us", href: "#" },
-      { id: 2, text: "Tourz Reviews", href: "#" },
-      { id: 3, text: "Contact Us", href: "#" },
-      { id: 4, text: "Travel Guides", href: "#" },
+      { id: 1, text: "About Us", href: "/our-vision" },
       { id: 5, text: "Data Policy", href: "#" },
-      { id: 6, text: "Cookie Policy", href: "#" },
       { id: 7, text: "Legal", href: "#" },
       { id: 8, text: "Sitemap", href: "#" },
     ],
@@ -15,10 +11,9 @@ const sections = [
   {
     title: "Support",
     links: [
-      { id: 9, text: "Get in Touch", href: "#" },
+      { id: 9, text: "Get in Touch", href: "/contact" },
       { id: 10, text: "Help center", href: "#" },
       { id: 11, text: "Live chat", href: "#" },
-      { id: 12, text: "How it works", href: "#" },
     ],
   },
 ];
